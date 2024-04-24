0df8cc9bb6f2002f16ddb98a3269a44095b95eb9
NamTHT <NamTHT@vmogroup.com>
2024-04-24 16:53:29 +0700

    feat: add i18n

--- podcast/src/main.tsx
+++ podcast/src/main.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
+import "./settings/localization/i18n.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
