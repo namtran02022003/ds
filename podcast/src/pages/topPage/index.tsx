1bcafb30a1a00925831ead6c6b2a16f087a6a4e6
namtht <namtht.if@gmail.com>
2024-05-12 09:01:36 +0700

    update: update home page

--- podcast/src/pages/topPage/index.tsx
+++ podcast/src/pages/topPage/index.tsx
@@ -1,6 +1,6 @@
 import { HeadPhoneIcon } from "@/assets/icons/HeadPhoneIcon";
 import i18n from "@/settings/localization/i18n";
-
+import explore_more_english_img from "@/assets/imgs/explore_more_english.svg";
 const HomePage = () => {
   return (
     <div>
@@ -94,6 +94,68 @@ const HomePage = () => {
           </div>
           <p>{i18n.t("1")}</p>
           <p>{i18n.t("2")}</p>
+        </div>
+      </div>
+      <div className="bg-[#174873] h-[500px] relative my-10">
+        <div className="text-center text-white py-10">
+          <h3 className="text-[40px] font-[400] leading-[48.41px]">
+            Explore More English Listening Activities
+          </h3>
+          <p className="text-[20px] font-[400] leading-[24.2px]">
+            Randall has created a variety of other ESL listening activities to
+            improve your comprehension, speaking, vocabulary, and cultural
+            awareness.
+          </p>
+        </div>
+        <div className="px-20 mx-auto">
+          <div className="grid grid-cols-3 gap-x-14">
+            <div className="rounded-[20px] text-center border-[#0077B5] p-5 border bg-white">
+              <img
+                className="pt-10 pb-20"
+                src={explore_more_english_img}
+                alt="des"
+              />
+              <p className="text-[20px] font-[700] leading-[24.2px]">Easy</p>
+              <p className="leading-[14.52px] font-[400] text-[12px] py-2">
+                Listening for high-beginning ESL studemts
+              </p>
+              <button className="py-5 text-[#0077B5] leading-[19.36px] font-[700] text-[16px]">
+                View Quizzes
+              </button>
+            </div>
+            <div className="rounded-[20px] text-center border-[#0077B5] p-5 border bg-white">
+              <img
+                className="pt-10 pb-20"
+                src={explore_more_english_img}
+                alt="des"
+              />
+              <p className="text-[20px] font-[700] leading-[24.2px]">
+                Intermediate
+              </p>
+              <p className="leading-[14.52px] font-[400] text-[12px] py-2">
+                Listening for high-beginning ESL studemts
+              </p>
+              <button className="py-5 text-[#0077B5] leading-[19.36px] font-[700] text-[16px]">
+                View Quizzes
+              </button>
+            </div>
+            <div className="rounded-[20px] text-center border-[#0077B5] p-5 border bg-white">
+              <img
+                className="pt-10 pb-20"
+                src={explore_more_english_img}
+                alt="des"
+              />
+              <p className="text-[20px] font-[700] leading-[24.2px]">
+                Difficult
+              </p>
+              <p className="leading-[14.52px] font-[400] text-[12px] py-2">
+                Listening for high-beginning ESL studemts
+              </p>
+              <button className="py-5 text-[#0077B5] leading-[19.36px] font-[700] text-[16px]">
+                View Quizzes
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
