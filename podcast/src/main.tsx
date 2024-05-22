b31dcecffb2ecde4722e6a2acb4ec6c336afc2ac
namtht <namtht.if@gmail.com>
2024-05-22 11:40:41 +0700

    feat: add READMY.MD file

--- podcast/src/main.tsx
+++ podcast/src/main.tsx
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./settings/localization/i18n.ts";
-
 import { RouterProvider } from "react-router-dom";
-
 import React from "react";
 import { router } from "./routes/index.tsx";
 
